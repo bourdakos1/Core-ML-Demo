b794b9bb035a93923df953d6c33d1aa338e6e6f3
Nick Bourdakos <bourdakos1@gmail.com>
2017-07-05 02:06:00 -0700

    start building better camera

--- VisualRecognition/VisualRecognition/CameraViewController.swift
+++ VisualRecognition/VisualRecognition/CameraViewController.swift
@@ -87,12 +87,49 @@ class CameraViewController: UIViewController, AVCapturePhotoCaptureDelegate {
             intent: .defaultIntent
         )
         
-        classify(cgImageRef!) { data in
-            // "push" pushes our data to our ResultsTableViewController.
-            self.push(data: data)
+//        classify(cgImageRef!) { data in
+//            // "push" pushes our data to our ResultsTableViewController.
+//            self.push(data: data)
+//        }
+        
+        let image = UIImage(data: photoData!)!
+        
+//        let image = UIImage(named: "dog.jpg")!
+        
+        let buffer = image.buffer()!
+        guard let output = try? YOLO().predict(image: buffer) else {
+            print("failed")
+            return
         }
         
-        let image = UIImage(data: photoData!)
+        for i in 0..<10 {
+            if i < output.count {
+                let prediction = output[i]
+                
+                let width = view.bounds.width
+                let height = width * 4 / 3
+                let scaleX = width / 416
+                let scaleY = height / 416
+                let top = (view.bounds.height - height) / 2
+                
+                // Translate and scale the rectangle to our own coordinate system.
+                var rect = prediction.rect
+                rect.origin.x *= scaleX
+                rect.origin.y *= scaleY
+                rect.origin.y += top
+                rect.size.width *= scaleX
+                rect.size.height *= scaleY
+                
+                let testView = UIView(frame: rect)
+                testView.backgroundColor = UIColor.red
+                testView.alpha=0.5
+                self.view.addSubview(testView)
+                
+                print(rect)
+                print(prediction.score * 100)
+            }
+        }
+        
         
         tempImageView.image = image
         tempImageView.isHidden = false
@@ -100,35 +137,37 @@ class CameraViewController: UIViewController, AVCapturePhotoCaptureDelegate {
     
     // Classification method.
     func classify(_ image: CGImage, completion: @escaping ([VNClassificationObservation]) -> Void) {
-        DispatchQueue.global(qos: .background).async {
-            // Initialize the coreML vision model, you can also use VGG16().model, or any other model that takes an image.
-            guard let vnCoreModel = try? VNCoreMLModel(for: Inceptionv3().model) else { return }
-            
-            // Build the coreML vision request.
-            let request = VNCoreMLRequest(model: vnCoreModel) { (request, error) in
-                // We get get an array of VNClassificationObservations back
-                // This has the fields "confidence", which is the score
-                // and "identifier" which is the recognized class
-                guard var results = request.results as? [VNClassificationObservation] else { fatalError("Failure") }
-                
-                // Filter out low scoring results.
-                results = results.filter({ $0.confidence > 0.01 })
-                
-                DispatchQueue.main.async {
-                    completion(results)
-                }
-            }
-            
-            // Initialize the coreML vision request handler.
-            let handler = VNImageRequestHandler(cgImage: image)
-            
-            // Perform the coreML vision request.
-            do {
-                try handler.perform([request])
-            } catch {
-                print("Error: \(error)")
-            }
-        }
+//        DispatchQueue.global(qos: .background).async {
+//            // Initialize the coreML vision model, you can also use VGG16().model, or any other model that takes an image.
+//            guard let vnCoreModel = try? VNCoreMLModel(for: Yolo().model) else { return }
+//
+//            // Build the coreML vision request.
+//            let request = VNCoreMLRequest(model: vnCoreModel) { (request, error) in
+//                // We get get an array of VNClassificationObservations back
+//                // This has the fields "confidence", which is the score
+//                // and "identifier" which is the recognized class
+//                guard var results = request.results as? [VNClassificationObservation] else { fatalError("Failure") }
+//
+//                print(results)
+//
+//                // Filter out low scoring results.
+////                results = results.filter({ $0.confidence > 0.01 })
+//
+////                DispatchQueue.main.async {
+////                    completion(results)
+////                }
+//            }
+//
+//            // Initialize the coreML vision request handler.
+//            let handler = VNImageRequestHandler(cgImage: image)
+//
+//            // Perform the coreML vision request.
+//            do {
+//                try handler.perform([request])
+//            } catch {
+//                print("Error: \(error)")
+//            }
+//        }
     }
     
     // Convenience method for closing the TableView.
@@ -163,17 +202,17 @@ class CameraViewController: UIViewController, AVCapturePhotoCaptureDelegate {
         captureButton.isHidden = true
         retakeButton.isHidden = false
         
-        // Show an activity indicator while its loading.
-        let alert = UIAlertController(title: nil, message: "Please wait...", preferredStyle: .alert)
-        
-        alert.view.tintColor = UIColor.black
-        let loadingIndicator: UIActivityIndicatorView = UIActivityIndicatorView(frame: CGRect(x: 10, y: 5, width: 50, height: 50)) as UIActivityIndicatorView
-        loadingIndicator.hidesWhenStopped = true
-        loadingIndicator.activityIndicatorViewStyle = UIActivityIndicatorViewStyle.gray
-        loadingIndicator.startAnimating()
-        
-        alert.view.addSubview(loadingIndicator)
-        present(alert, animated: true, completion: nil)
+//        // Show an activity indicator while its loading.
+//        let alert = UIAlertController(title: nil, message: "Please wait...", preferredStyle: .alert)
+//
+//        alert.view.tintColor = UIColor.black
+//        let loadingIndicator: UIActivityIndicatorView = UIActivityIndicatorView(frame: CGRect(x: 10, y: 5, width: 50, height: 50)) as UIActivityIndicatorView
+//        loadingIndicator.hidesWhenStopped = true
+//        loadingIndicator.activityIndicatorViewStyle = UIActivityIndicatorViewStyle.gray
+//        loadingIndicator.startAnimating()
+//
+//        alert.view.addSubview(loadingIndicator)
+//        present(alert, animated: true, completion: nil)
     }
     
     @IBAction func retake() {
@@ -181,5 +220,26 @@ class CameraViewController: UIViewController, AVCapturePhotoCaptureDelegate {
         captureButton.isHidden = false
         retakeButton.isHidden = true
         dismissResults()
+    }
+}
+
+extension UIImage {
+    
+    func buffer() -> CVPixelBuffer? {
+        var pixelBuffer: CVPixelBuffer? = nil
+        
+        let width = Int(416)
+        let height = Int(416)
+        
+        let attrs = [kCVPixelBufferCGImageCompatibilityKey: kCFBooleanTrue, kCVPixelBufferCGBitmapContextCompatibilityKey: kCFBooleanTrue] as CFDictionary
+        CVPixelBufferCreate(kCFAllocatorDefault, width, height, kCVPixelFormatType_32ARGB, attrs, &pixelBuffer)
+        CVPixelBufferLockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue:0))
+        
+        let colorspace = CGColorSpaceCreateDeviceRGB()
+        let bitmapContext = CGContext(data: CVPixelBufferGetBaseAddress(pixelBuffer!), width: width, height: height, bitsPerComponent: 8, bytesPerRow: CVPixelBufferGetBytesPerRow(pixelBuffer!), space: colorspace, bitmapInfo: CGImageAlphaInfo.noneSkipFirst.rawValue)!
+        
+        bitmapContext.draw(self.cgImage!, in: CGRect(x: 0, y: 0, width: width, height: height))
+        
+        return pixelBuffer
     }
 }
